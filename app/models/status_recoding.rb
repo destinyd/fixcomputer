5367676bfdb3ae5efd80f0c5c4122f8664f9be49
destinyd <destinyd_war@163.com>
2013-03-25 21:15:46 +0800

    status recoding default scope

--- app/models/status_recoding.rb
+++ app/models/status_recoding.rb
@@ -5,4 +5,7 @@ class StatusRecoding
   belongs_to :problem
   symbolize :status,in: [:contacted,:visited,:token,:repaired,:paid,:finish,:cancel],default: :contacted
   field :plus, type: String
+
+  scope :recent,order_by(:created_at => :desc)
+  default_scope recent
 end
