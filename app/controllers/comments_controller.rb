09272220965159d671ba572784d8eeb3404a3789
destinyd <destinyd_war@163.com>
2013-04-04 02:37:44 +0800

    for ie6

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,11 @@
 class CommentsController < InheritedResources::Base
   respond_to :js, :only => :create
   actions :all,except: [:index,:show,:edit,:update,:new,:destroy]
+
+  def create
+    create! do |format|
+      format.html{ redirect_to root_url}
+      format.js
+    end
+  end
 end
